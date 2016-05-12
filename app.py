e83b7883043ab4e2d2be498fcedb57bec57b53cf
Carla Suarez <csuarez@thoughtworks.com>
2016-05-12 09:58:18 -0500

    Add Health Check for example app

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hola Mundo'
 
+@app.route('/example/HealthCheck')
+def hello_world():
+    return 'Example is Healthy'
+
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
